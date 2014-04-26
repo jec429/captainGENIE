a7e311d6d56fa021357492a8f387a05b8bccb3cb
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-04-26 17:24:36 -0400

    Adapt scripts to work with captainGENIE.

--- scripts/run-captain-genie.sh
+++ scripts/run-captain-genie.sh
@@ -5,7 +5,7 @@
 # which is lightly customized from gevgen.  
 
 # The number of events to generate
-EVENTS=5000
+EVENTS=50
 
 # The output file name.
 GHEP_RUN=1
@@ -14,12 +14,18 @@ GHEP_FILE="${GHEP_PREFIX}.${GHEP_RUN}.ghep.root"
 
 # The flux to use.  GENIE assumes the energy is in units of GeV, so
 # the peak energy is 5 GeV.
-FLUX="text,14,../flux/fluka08_me000z200i_810km_0kmoa_flux.txt,0,2"
-FLUX="${FLUX}:text,-14,../flux/fluka08_me000z200i_810km_0kmoa_flux.txt,0,5"
+# FLUX="text,14,../flux/fluka08_me000z200i_810km_0kmoa_flux.txt,0,2"
+# FLUX="${FLUX}:text,-14,../flux/fluka08_me000z200i_810km_0kmoa_flux.txt,0,5"
 
- gevgen_capt.exe -r ${GHEP_RUN} \
+# Set a monoenergetic flux
+# FLUX="mono,14,2"
+
+# Set a functional flux
+FLUX="func,14,x*exp(-x),100,0.1,10"
+
+gdb --args gevgen_capt.exe -r ${GHEP_RUN} \
      -n ${EVENTS} \
      -f ${FLUX} 
   
-gntpc -f rootracker -i ${GHEP_FILE} -o captain-genie.root
+# gntpc -f rootracker -i ${GHEP_FILE} -o captain-genie.root
 
